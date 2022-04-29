eabf61b2f1b05d9f93405495eeb19f4d24fad256
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2022-04-29 19:04:17 -0400

    Make separate setup script for private subcommands

--- manage.py
+++ manage.py
@@ -1,27 +1,20 @@
+from argparse import _SubParsersAction
+
 import argparse
 import config
 import data.sync as sync
-import data.data.archive.sync as sync_archive
-import data.data.archive.migrate as migrate_archive
-import data.data.randomize_names.make_mapping as make_mapping
-import data.data.randomize_names.apply_mapping as apply_mapping
 import prediction.predict as predict
 
 
-def _sync_archive(args) -> None:
-    sync_archive.main()
-
-
-def _migrate_archive(args) -> None:
-    migrate_archive.main(args.file)
-
-
-def _make_name_mapping(args) -> None:
-    make_mapping.main()
-
-
-def _apply_name_mapping(args) -> None:
-    apply_mapping.main()
+def _try_add_private_subparsers(subparsers: _SubParsersAction) -> None:
+    """
+    Adds subparsers for the private subcommands if the data/data submodule is present, otherwise does nothing.
+    """
+    try:
+        from data.data.manage import add_subparsers
+        add_subparsers(subparsers)
+    except ModuleNotFoundError:
+        pass
 
 
 def main():
@@ -36,19 +29,8 @@ def main():
     predict_parser.add_argument("--game", "-g", type=int, default=-1, help="Game for which to make the prediction")
     predict_parser.add_argument("--round", "-r", type=int, default=-1, help="Number of rounds to use in the prediction.")
     predict_parser.set_defaults(func=predict.main)
-    # 'sync-archive' subcommand (private)
-    archive_sync_parser = subparsers.add_parser("sync-archive")
-    archive_sync_parser.set_defaults(func=_sync_archive)
-    # 'migrate-archive' subcommand (private)
-    archive_migrate_parser = subparsers.add_parser("migrate-archive")
-    archive_migrate_parser.add_argument("--file", "-f", required=False, default=config.WORKBOOK_NAME)
-    archive_migrate_parser.set_defaults(func=_migrate_archive)
-    # 'make-name-mapping' subcommand (private)
-    map_names_parser = subparsers.add_parser("make-name-mapping")
-    map_names_parser.set_defaults(func=_make_name_mapping)
-    # 'apply-name-mapping' subcommand (private)
-    shuffle_names_parser = subparsers.add_parser("apply-name-mapping")
-    shuffle_names_parser.set_defaults(func=_apply_name_mapping)
+    # Private subcommands
+    _try_add_private_subparsers(subparsers)
     # Parse arguments
     args = parser.parse_args()
     args.func(args)
